80023a3ea4a1f23a4a32d4c7c6d56fc04ea13a03
Fabian Kędzierski <grafska@gmail.com>
2016-11-05 21:02:37 +0100

    Add 'Item' resources

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,31 @@
+class ItemsController < ApplicationController
+  expose :items, -> { Item.all }
+  expose :item
+
+  def create
+    if item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    item.destroy
+    redirect_to items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description)
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
-  has_many :item_users
-  has_many :expenses
-  has_many :payments
+  has_many :item_users, dependent: :destroy
+  has_many :expenses, dependent: :destroy
+  has_many :payments, dependent: :destroy
+
+  validates :name, presence: true
 end
