ed4cb7658e37b4a9ce00b546671625023dee8a7c
Fabian Kędzierski <grafska@gmail.com>
2016-12-12 13:44:53 +0100

    Change expense_section in items#show (#14)
    * Set 'null:false' for ExpenseUser fields
    
    * Change expense_section in items#show
    
    * Clean code
    
    * Remove unused parameter

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202192538) do
+ActiveRecord::Schema.define(version: 20161212122550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "expense_users", force: :cascade do |t|
-    t.integer  "expense_id"
-    t.integer  "user_id"
+    t.integer  "expense_id", null: false
+    t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["expense_id"], name: "index_expense_users_on_expense_id", using: :btree
